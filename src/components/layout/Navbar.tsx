64511f335672ce63c3d9c14897e86aa60ad4a65c
ArnthorDadi <arnthordj@gmail.com>
2023-07-04 13:17:58 +0000

    Added apparatus selector in navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { ApparatusList } from "@src/elements/types";
+import { Apparatus, ApparatusList } from "@src/elements/types";
 import { Page } from "@src/utils/Page";
 import { cn } from "@src/utils/Utility";
+import { usePathname } from "next/navigation";
 
 type NavbarProps = {
   className?: string;
 };
 
 export const Navbar: React.FC<NavbarProps> = ({ className }) => {
+  const selectedApparatus = useGetApparatusSelected();
+
   return (
     <nav
       style={{ borderBottom: "1px solid #176B87" }}
@@ -30,9 +33,11 @@ export const Navbar: React.FC<NavbarProps> = ({ className }) => {
       <div className={"flex flex-row gap-4"}>
         {ApparatusList.map((apparatus) => (
           <a
-            className={
-              "rounded-full px-4 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-            }
+            key={apparatus}
+            className={cn(
+              "rounded-full px-4 py-3 font-semibold text-white no-underline transition hover:bg-white/20",
+              selectedApparatus === apparatus && "bg-white/10"
+            )}
             href={`/apparatus/${apparatus}`}
           >
             {apparatus.slice(0, 1).toUpperCase() +
@@ -43,3 +48,41 @@ export const Navbar: React.FC<NavbarProps> = ({ className }) => {
     </nav>
   );
 };
+
+function useGetApparatusSelected() {
+  const pathname = usePathname();
+
+  const [selectedApparatus, setSelectedApparatus] = useState<
+    Apparatus | undefined
+  >(undefined);
+
+  useEffect(() => {
+    const splitPathname = pathname?.split("/");
+    const lastPathname = splitPathname?.[splitPathname?.length - 1];
+
+    switch (lastPathname) {
+      case Apparatus.floor:
+        setSelectedApparatus(Apparatus.floor);
+        break;
+      case Apparatus.pommel_horse:
+        setSelectedApparatus(Apparatus.pommel_horse);
+        break;
+      case Apparatus.rings:
+        setSelectedApparatus(Apparatus.rings);
+        break;
+      case Apparatus.vault:
+        setSelectedApparatus(Apparatus.vault);
+        break;
+      case Apparatus.parallel_bars:
+        setSelectedApparatus(Apparatus.parallel_bars);
+        break;
+      case Apparatus.high_bar:
+        setSelectedApparatus(Apparatus.high_bar);
+        break;
+      default:
+        setSelectedApparatus(undefined);
+        break;
+    }
+  }, [pathname]);
+  return selectedApparatus;
+}
